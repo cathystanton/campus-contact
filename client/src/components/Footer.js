5c897fb5b6b6c76321293c3915d163aab9a06bec
cathystanton <cjstanton@college.harvard.edu>
2022-02-27 14:53:22 -0500

    first footer social logo

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -9,10 +9,23 @@ function Footer() {
         <Link className="footer-link" to="/about">About</Link>
       </box>
       <box>
+        <Link className="footer-link" to="/#">Terms of Service</Link>
+      </box>
+      <box>
+        <Link className="footer-link" to="/#">Privacy Policy</Link>
+      </box>
+      <box>
+        <Link className="footer-link" to="/#">Contact</Link>
+      </box>
+      <box>
         <Link className="footer-link" to="https://voteschallenge.harvard.edu/">Harvard Votes Challenge</Link>
       </box>
       <box>
         <Link className="footer-link" to="https://iop.harvard.edu/">Harvard IOP</Link>
+      </box>
+      <br />
+      <box>
+        <Link><image src="/icons/instagram.png" /></Link>
       </box>
     </footer>
   );
